ffb438e12826d9485d66b74dcfbe96e4d99ebc2c
drserafin <drserafin@csu.fullerton.edu>
2024-11-26 15:00:58 -0800

    reconfigure eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginJest from "eslint-plugin-jest";  // Import the Jest plugin
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,  // Node.js globals (e.g., `require`, `module`)
-        ...globals.jest,  // Jest globals (e.g., `describe`, `it`, `expect`, etc.)
-      },
+/** @type {import('eslint').Linter.Config} */
+export default {
+  languageOptions: {
+    globals: {
+      ...globals.node,  // Node.js globals (e.g., `require`, `module`)
+      ...globals.jest,  // Jest globals (e.g., `describe`, `it`, `expect`, etc.)
     },
   },
-  pluginJs.configs.recommended,  // Use the ESLint recommended JavaScript rules
-  {
-    plugins: {
-      jest: pluginJest,  // Define the Jest plugin as an object
-    },
-    rules: {
-      ...pluginJest.configs.recommended.rules,  // Directly include Jest's recommended rules
-    },
+  plugins: {
+    jest: pluginJest,  // Define the Jest plugin as an object
   },
-];
+  rules: {
+    ...pluginJs.configs.recommended.rules,  // Use ESLint recommended JavaScript rules
+    ...pluginJest.configs.recommended.rules,  // Use Jest's recommended rules
+  },
+};
