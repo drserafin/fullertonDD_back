819407969f93f261e45b36f4c9c2366024e5870e
drserafin <drserafin@csu.fullerton.edu>
2024-11-27 12:20:12 -0800

    modified ci/ci pipeline

--- src/models/users/User.js
+++ src/models/users/User.js
@@ -1,51 +1,30 @@
-const Sequelize = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/database');
-const passwordUtil = require('../../utils/passwordUtil');  // Correct path to the passwordUtils file
+const { hashPassword, comparePasswords } = require('../../utils/passwordUtil');  // Correct path to the passwordUtils file
 
 const User = sequelize.define('User', {
   user_id: {
-    type: Sequelize.DataTypes.INTEGER,
+    type: DataTypes.INTEGER,
     primaryKey: true,
-    autoIncrement: true
-  },
-  username: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    }
+    autoIncrement: true,
   },
   email: {
-    type: Sequelize.DataTypes.STRING,
+    type: DataTypes.STRING,
     allowNull: false,
     unique: true,
     validate: {
-      isEmail: true,
-    }
+      isEmail: true, // Ensure the provided value is a valid email
+    },
   },
-  password: {
-    type: Sequelize.DataTypes.STRING,
+  username: {
+    type: DataTypes.STRING,
     allowNull: false,
-    validate: {
-      len: [6, 100],
-    }
-  }
+  },
 }, {
   timestamps: true,
   createdAt: 'created_at',
   updatedAt: 'updated_at',
 });
 
-// Hash password before saving user instance using passwordUtils
-User.beforeCreate(async (user) => {
-  if (user.password) {
-    user.password = await passwordUtil.hashPassword(user.password);  // Use the hashPassword function from passwordUtils
-  }
-});
-
-User.prototype.isValidPassword = async function (password) {
-  return await passwordUtil.comparePasswords(password, this.password);  // Use the comparePasswords function from passwordUtils
-};
-
-// Exporting as an object to match the import in controllers
+// Export the model
 module.exports = User;
